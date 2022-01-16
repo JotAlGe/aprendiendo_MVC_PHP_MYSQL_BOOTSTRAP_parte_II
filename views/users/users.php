eeecf7fb096c53d09f35fcb8149d18d07bb6db0b
Juanchis <juanchismo10@gmail.com>
2022-01-15 23:43:28 -0300

    eliminación del botón de editar usuario en el listado de los mismos

--- views/users/users.php
+++ views/users/users.php
@@ -49,7 +49,7 @@
                                             <span class="label label-default"><?php echo $_SESSION['users_list'][$i]['birthday']; ?></span>
                                         </td>
                                         <td>
-                                            <a href="#"><?php echo $_SESSION['users_list'][$i]['email_user']; ?></a>
+                                            <a href="mailto:<?php echo $_SESSION['users_list'][$i]['email_user']; ?>"><?php echo $_SESSION['users_list'][$i]['email_user']; ?></a>
                                         </td>
                                         <td style="width: 20%;">
                                             <a href="?controller=users&action=index&id=<?php echo $_SESSION['users_list'][$i]['id_user']; ?>    " class="table-link text-warning">
@@ -58,12 +58,7 @@
                                                     <i class="fa fa-search-plus fa-stack-1x fa-inverse"></i>
                                                 </span>
                                             </a>
-                                            <a href="#" class="table-link text-info">
-                                                <span class="fa-stack">
-                                                    <i class="fa fa-square fa-stack-2x"></i>
-                                                    <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
-                                                </span>
-                                            </a>
+
                                             <?php if ($_SESSION['users_list'][$i]['cod_level'] != 1) { ?>
                                                 <a onclick="if(confirm('¿Desea eliminar el este post?')){ return true;} else{return false;}" href="?controller=users&action=delete&id=<?php echo $_SESSION['users_list'][$i]['id_user']; ?>" class="table-link danger">
                                                     <span class="fa-stack">
